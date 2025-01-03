67928d3020af51daec32331965815ff9938ab30b
nhansp <nhan.094.vn@gmail.com>
2025-01-04 03:27:00 +0700

    add pixel-style and pixel-framework
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- all.mk
+++ all.mk
@@ -1,4 +1,4 @@
-# 2025 tvyiutnhisokewt
+\# 2025 tvyiutnhisokewt
 
 # Required makefiles. Also hard include pif.mk to avoid null pihook variables.
 include vendor/tvyiutnhisokewt/configs/makefile/vars.mk
@@ -6,3 +6,8 @@ include vendor/tvyiutnhisokewt/configs/makefile/props.mk
 include vendor/tvyiutnhisokewt/configs/makefile/pif.mk
 include vendor/tvyiutnhisokewt/configs/makefile/fonts.mk
 
+# Pixel style
+$(call inherit-product, vendor/tvyiutnhisokewt/pixel-style/config/common.mk)
+
+# Pixel framework
+$(call inherit-product, vendor/tvyiutnhisokewt/pixel-framework/config.mk)
